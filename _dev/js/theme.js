0e703450d959d06777eefec53f4b5a6742835262
trichier <>
2023-01-13 15:36:02 +0100

    #00004 Mise en place du JS

--- _dev/js/theme.js
+++ _dev/js/theme.js
@@ -7,4 +7,41 @@ window.addEventListener('load', () => {
     body.removeChild(preloader);
 })
 
-/* =============== PRELOADER =============== */
+/*=============== MODAL ===============*/
+const modal_View = document.querySelectorAll('.portfolio__modal');
+const modal_Button = document.querySelectorAll('.portfolio__button.modal');
+const modal_Close = document.querySelectorAll('.portfolio__modal-close');
+
+let modal = (Click) =>{
+    modal_View[Click].classList.toggle('is-active');
+}
+modal_Button.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        modal(i);
+    })
+});
+modal_Close.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        modal(i);
+    })
+});
+
+/*=============== SCROLL NAV ACTIVE LINK ===============*/
+const section = document.querySelectorAll('section[id]');
+function scrollNavMenuActive() {
+
+    const scrollWindowY = document.documentElement.scrollTop;
+
+    section.forEach((current_Section) =>{
+        const section_Height = current_Section.clientHeight;
+        const section_Top = current_Section.offsetTop - 105;
+        const section_Id = current_Section.getAttribute('id');
+
+        if( scrollWindowY > section_Top && scrollWindowY <= section_Top + section_Height ){
+            document.querySelector(`.nav__menu a[href*=${section_Id}]`).classList.add('is-active');
+        }else{
+            document.querySelector(`.nav__menu a[href*=${section_Id}]`).classList.remove('is-active');
+        }
+    })
+}
+window.addEventListener('scroll', scrollNavMenuActive);
