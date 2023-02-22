fd3cd887a6978fd41ae20e7be181509ff1d5d223
trichier <>
2023-02-23 00:17:42 +0100

    Sortie de la V2

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Portfolio
+# Mon Portfolio
+
+Bienvenue sur mon portfolio ! Vous pouvez y trouver mes projets personnels et professionnels, mes compétences et mes expériences.
+
+## Projets
+
+- Projet 1 : [Nom du projet](lien github)
+- Projet 2 : [Nom du projet](lien github)
+- Projet 3 : [Nom du projet](lien github)
+
+## Compétences
+
+- Français
+- Anglais
+
+## Expériences
+
+- Advisa : [Apprentie Développeur Front-end Wordpress et Prestashop](https://www.linkedin.com/company/advisa-digital-agency/mycompany/)
+
+N'hésitez pas à consulter ma maquette [ici](https://www.figma.com/file/vRGSjMTPMIobP0kofQjcAn/Untitled?node-id=0%3A1&t=e6WiybfaCFZoLkEE-1) pour une présentation visuelle de mon portfolio.
+
+Merci de votre visite !
--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,1 +1,60 @@
-(()=>{var e,o,t,c,n,l;function r(e){t[e].classList.toggle("is-active")}e=document.querySelector("#preloader"),o=document.querySelector("body"),window.addEventListener("load",function(){e.classList.add("deleted"),o.removeChild(e)}),t=document.querySelectorAll(".portfolio__modal"),c=document.querySelectorAll(".portfolio__button.modal"),n=document.querySelectorAll(".portfolio__modal-close"),c.forEach(function(e,o){e.addEventListener("click",function(){r(o)})}),n.forEach(function(e,o){e.addEventListener("click",function(){r(o)})}),l=document.querySelectorAll("section[id]"),window.addEventListener("scroll",function(){var c=document.documentElement.scrollTop;l.forEach(function(e){var o=e.clientHeight,t=e.offsetTop-105,e=e.getAttribute("id");t<c&&c<=t+o?document.querySelector(".nav__menu a[href*=".concat(e,"]")).classList.add("is-active"):document.querySelector(".nav__menu a[href*=".concat(e,"]")).classList.remove("is-active")})})})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/theme.js":
+/*!*********************!*\
+  !*** ./js/theme.js ***!
+  \*********************/
+/***/ (() => {
+
+eval("/* =============== PRELOADER =============== */\nvar preloader = document.querySelector('#preloader');\nvar body = document.querySelector('body');\nwindow.addEventListener('load', function () {\n  preloader.classList.add('deleted');\n  body.removeChild(preloader);\n});\n/*=============== MODAL ===============*/\n\nvar modal_View = document.querySelectorAll('.portfolio__modal');\nvar modal_Button = document.querySelectorAll('.portfolio__button.modal');\nvar modal_Close = document.querySelectorAll('.portfolio__modal-close');\n\nvar modal = function modal(Click) {\n  modal_View[Click].classList.toggle('is-active');\n};\n\nmodal_Button.forEach(function (btn, i) {\n  btn.addEventListener('click', function () {\n    modal(i);\n  });\n});\nmodal_Close.forEach(function (btn, i) {\n  btn.addEventListener('click', function () {\n    modal(i);\n  });\n});\n/*=============== SCROLL NAV ACTIVE LINK ===============*/\n\nvar section = document.querySelectorAll('section[id]');\n\nfunction scrollNavMenuActive() {\n  var scrollWindowY = document.documentElement.scrollTop;\n  section.forEach(function (current_Section) {\n    var section_Height = current_Section.clientHeight;\n    var section_Top = current_Section.offsetTop - 105;\n    var section_Id = current_Section.getAttribute('id');\n\n    if (scrollWindowY > section_Top && scrollWindowY <= section_Top + section_Height) {\n      document.querySelector(\".nav__menu a[href*=\".concat(section_Id, \"]\")).classList.add('is-active');\n    } else {\n      document.querySelector(\".nav__menu a[href*=\".concat(section_Id, \"]\")).classList.remove('is-active');\n    }\n  });\n}\n\nwindow.addEventListener('scroll', scrollNavMenuActive);\n\n//# sourceURL=webpack://sass/./js/theme.js?");
+
+/***/ }),
+
+/***/ "./css/theme.scss":
+/*!************************!*\
+  !*** ./css/theme.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://sass/./css/theme.scss?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_modules__["./js/theme.js"](0, {}, __webpack_require__);
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./css/theme.scss"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
